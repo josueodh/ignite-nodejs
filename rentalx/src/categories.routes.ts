72cd34b2da587a7c0b99399d3c76e9e0845982b3
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-03-04 00:29:26 -0300

    (Cap 2) - Rentalx - Aula 1.2.4

--- rentalx/src/categories.routes.ts
+++ rentalx/src/categories.routes.ts
@@ -1,18 +1,21 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
 
 const categoriesRouter = Router();
 
 const categories = [];
 
-categoriesRouter.post("/categories", (request, response) => {
+categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuid(),
     name,
     description,
-  });
+  };
+  categories.push(category);
 
-  return response.status(201).json(categories);
+  return response.status(201).json(category);
 });
 
 export { categoriesRouter };
--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRouter);
+app.use("/categories", categoriesRouter);
 
 app.listen(3333, () => console.log("Server is running!"));
