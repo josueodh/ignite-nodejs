a2e63342e9e2160f7e44cfd2ba1d6d7cb7a89bd0
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-03-04 00:26:03 -0300

    (Cap 2) - Rentalx - Aula 1.2.3

---
+++ rentalx/src/categories.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+const categoriesRouter = Router();
+
+const categories = [];
+
+categoriesRouter.post("/categories", (request, response) => {
+  const { name, description } = request.body;
+
+  categories.push({
+    name,
+    description,
+  });
+
+  return response.status(201).json(categories);
+});
+
+export { categoriesRouter };
